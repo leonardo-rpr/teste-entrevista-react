139c354e1b950c2f669fed77de91e6692752739e
Leonardo Pinheiro <leo.r.pinheiro@hotmail.com>
2019-08-03 04:17:11 -0300

    add (project) storybook config

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
+import { addDecorator } from "@storybook/react";
+
+addDecorator(withKnobs);
+
+const req = require.context("../src/app/components", true, /\.stories\.ts$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
