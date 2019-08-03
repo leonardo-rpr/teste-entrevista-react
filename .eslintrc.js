f2d3f6bc1775cfdbef1172f7b3b855b500631567
Leonardo Pinheiro <leo.r.pinheiro@hotmail.com>
2019-08-03 04:32:53 -0300

    add (project) configure lint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    "react-app",
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "import", "jsx-a11y"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"]
+      }
+    ],
+    "no-underscore-dangle": "off",
+    "object-curly-newline": "off",
+    "jsx-quotes": "off",
+    "import/prefer-default-export": "off",
+    "react/prop-types": "off",
+    semi: "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off"
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      typescript: {}
+    }
+  }
+};
